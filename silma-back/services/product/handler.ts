c7b4b06bea8db14d3e09102d4f47b6a7d2c9a1e8
JuanPabdl <a00825316@itesm.mx>
2023-04-05 15:48:28 -0600

    feat: Corrected logic to Add Product

--- silma-back/services/product/handler.ts
+++ silma-back/services/product/handler.ts
@@ -15,7 +15,27 @@ const createProductFunction: SilmaAPIFunction = async(
     }
 
     const prodDB: ProductAttributes = {
-        //Atributos de producto actualizados
+        createdAt: new Date(),
+        title: data.title,
+        author: data.author,
+        type: data.type,
+        synopsis: data.synopsis,
+        salesPrice: data.salesPrice,
+        authorPrice: data.authorPrice,
+        gender: data.gender,
+        language: data.language,
+        format: data.format,
+        numberPages: data.numberPages,
+        suggestedAges: data.suggestedAges,
+        weight: data.weight,
+        dimensions: data.dimensions,
+        internalCode: data.internalCode,
+        isbn: data.isbn,
+        quantity: data.quantity,
+        publicationYear: data.publicationYear,
+        edition: data.edition,
+        imageUrl: data.imageUrl,
+        status: data.status
     };
 
     const db = await connectToDatabase();
--- silma-back/types/Product.ts
+++ silma-back/types/Product.ts
@@ -2,11 +2,37 @@ import Joi from 'joi';
 import { longText } from 'utils'; 
 
 export type ProductCreate = {
-    createdAt?: Date;
+    title: string,
+    author: string,
+    type: ProductType,
+    synopsis: string,
+    quantity: number,
+    salesPrice: number,
+    authorPrice: number,
+    gender: ProductGender,
+    language: ProductLanguage,
+    format: ProductFormat,
+    numberPages: number,
+    suggestedAges: string,
+    weight: number,
+    dimensions: string,
+    internalCode: string,
+    isbn: string,
+    publicationYear: string,
+    edition: string,
+    imageUrl: string,
+    status: ProductStatus,
 };
 
 export const ProductCreateSchema = Joi.object<ProductCreate>({
-    createdAt: Joi.date().optional().label("Creation date"),
+    quantity: Joi.number().min(0),
+    salesPrice: Joi.number().min(0),
+    authorPrice: Joi.number().optional().min(0),
+    numberPages: Joi.number().optional().min(10),
+    weight: Joi.number().optional().min(1)
+
+    //ISBN (?): que cumpla con un patron estandar
+    //Genero: que el genero sea un arreglo que puede tener de 1 a 3 valores (libro) o sea nulo (articulo)
 });
 
 export const productTypes = ["book", "sticker", "stickerPack"] as const;
