a5a1ba3f2843c2610d69ca9fd5f157c6f51f2dee
gpaez-ol <tavo@outlandhq.com>
2023-04-06 10:56:12 -0600

    feat: Added Get In Orders Function

--- silma-back/services/inOrder/handler.ts
+++ silma-back/services/inOrder/handler.ts
@@ -2,7 +2,7 @@ import { APIGatewayEvent, APIGatewayProxyHandler } from "aws-lambda";
 import { InOrderAttributes, ProductInOrderAttributes } from "database/models";
 import { connectToDatabase } from "database/sequelize";
 import { SilmaAPIFunction, silmaAPIhandler } from "lib/handler/handler";
-import { InOrderCreate, InOrderCreateSchema } from "types";
+import { InOrderCreate, InOrderCreateSchema, InOrderItem } from "types";
 import { badRequest } from "utils";
 
 const createInOrderFunction: SilmaAPIFunction = async (
@@ -16,6 +16,7 @@ const createInOrderFunction: SilmaAPIFunction = async (
 
   const inOrderDB: InOrderAttributes = {
     createdAt: new Date(),
+    orderedAt: data.orderedAt,
     deliveredAt: data.deliveredAt ?? null,
     notes: data.notes,
     deletedAt: null,
@@ -35,9 +36,45 @@ const createInOrderFunction: SilmaAPIFunction = async (
     }
   );
   await ProductInOrder.bulkCreate(productOrderDb);
+  // TODO: map to the version needed
   return { data: newInOrder };
+};
+const getInOrdersFunction: SilmaAPIFunction = async (
+  event: APIGatewayEvent
+) => {
+  const db = await connectToDatabase();
+  const { InOrder, ProductInOrder, Product } = db;
+  const rawProductInOrders = await ProductInOrder.findAll({
+    include: [
+      { model: InOrder, attributes: ["id", "orderedAt", "deliveredAt"] },
+      { model: Product, attributes: ["id", "title"] },
+    ],
+    where: { deletedAt: null },
+    group: ["InOrder.id", "ProductInOrder.id", "Product.id"],
+    order: [["createdAt", "ASC"]],
+  });
+  const productInOrders = rawProductInOrders.map((rawInOrder) =>
+    rawInOrder.get({ plain: true })
+  );
+  const inOrderItems: InOrderItem[] = productInOrders.map((productInOrder) => {
+    return {
+      orderedAt: productInOrder.InOrder.orderedAt,
+      deliveredAt: productInOrder.InOrder.deliveredAt,
+      products: [
+        {
+          amount: productInOrder.amount,
+          id: productInOrder.Product.id,
+          title: productInOrder.Product.title,
+        },
+      ],
+    };
+  });
+
+  return { data: inOrderItems };
 };
 
 export const createInOrder: APIGatewayProxyHandler = silmaAPIhandler(
   createInOrderFunction
 );
+export const getInOrders: APIGatewayProxyHandler =
+  silmaAPIhandler(getInOrdersFunction);
