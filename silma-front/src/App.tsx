9c7a9f99637bff180b7d0c961af632aa34fd90b7
Estefania Charles <61810430+estefycharles@users.noreply.github.com>
2023-04-14 03:53:42 -0600

    tabla de libros, surtido interno y orden de entrada

--- silma-front/src/App.tsx
+++ silma-front/src/App.tsx
@@ -3,12 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './pages/Login';
+import Products from './pages/Products';
+import Storage from './pages/Storage';
+import InOrder from './pages/InOrder';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login/>} />
+        <Route path="/inicio-sesion" element={<Login/>} />
+        <Route path="/productos" element={<Products/>} />
+        <Route path="/surtido-interno" element={<Storage/>} />
+        <Route path="/orden-entrada" element={<InOrder/>} />
       </Routes>
     </Router>
   );
