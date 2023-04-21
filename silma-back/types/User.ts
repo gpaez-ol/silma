8648bf86ed780db39b47c876eea4a7105e8d0d15
gpaez-ol <tavo@outlandhq.com>
2023-04-21 13:50:50 -0600

    feat: Added UserLogin types

--- silma-back/types/User.ts
+++ silma-back/types/User.ts
@@ -1,2 +1,15 @@
+import Joi from "joi";
+import { longText } from "utils";
+
 export const userTypes = ["user", "admin"] as const;
 export type UserType = typeof userTypes[number];
+
+export type UserLogin = {
+  email: string;
+  password: string;
+};
+
+export const UserLoginSchema = Joi.object<UserLogin>({
+  email: Joi.string().required().max(longText).label("Email"),
+  password: Joi.string().required().max(longText).label("Password"),
+});
