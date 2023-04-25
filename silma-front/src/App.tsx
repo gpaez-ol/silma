dffaadd86bc84192b591354d3fcfe47a5068812c
anaelisa8 <anaelisaestrada@gmail.com>
2023-04-25 01:31:05 -0600

    feat: Added login back with front connection

--- silma-front/src/App.tsx
+++ silma-front/src/App.tsx
@@ -3,10 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './pages/Login';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <Router>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        //hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover
+        theme="dark"
+      />
       <Routes>
         <Route path="/login" element={<Login/>} />
       </Routes>
