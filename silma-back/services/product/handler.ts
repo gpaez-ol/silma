0665f0a7c27d83640e2d3880adde8836188691ab
gpaez-ol <tavo@outlandhq.com>
2023-05-01 17:57:11 -0600

    fix: Refactored Create Product to use optional items and to build internal code with custom logic

--- silma-back/services/product/handler.ts
+++ silma-back/services/product/handler.ts
@@ -3,45 +3,56 @@ import { ProductAttributes } from "database/models";
 import { connectToDatabase } from "database/sequelize";
 import { SilmaAPIFunction, silmaAPIhandler } from "lib/handler/handler";
 import { ProductCreate, ProductCreateSchema } from "types";
-import { getArticlesList, getBooksList } from "logic/product";
+import {
+  buildInternalCode,
+  getArticlesList,
+  getBooksList,
+} from "logic/product";
 import { badRequest } from "utils";
 
 const createProductFunction: SilmaAPIFunction = async (
   event: APIGatewayEvent
 ) => {
   const data: ProductCreate = JSON.parse(event.body);
-  const {error} = ProductCreateSchema.validate(data);
+  const { error } = ProductCreateSchema.validate(data);
   if (error) {
-    throw badRequest("Wrong data format");
+    throw badRequest(error.message);
   }
-  
+  const db = await connectToDatabase();
+  const { Product } = db;
+  const itemNumber = await Product.findAndCountAll({
+    where: { author: data.author ?? null },
+  });
+  const internalCode = buildInternalCode(
+    ++itemNumber.count,
+    data.author ?? null
+  );
+
   const prodDB: ProductAttributes = {
     createdAt: new Date(),
     title: data.title,
-    author: data.author,
     type: data.type,
     synopsis: data.synopsis,
     salesPrice: data.salesPrice,
-    authorPrice: data.authorPrice,
-    genre: data.genre,
-    language: data.language,
-    format: data.format,
-    numberPages: data.numberPages,
-    suggestedAges: data.suggestedAges,
-    weight: data.weight,
-    dimensions: data.dimensions,
-    internalCode: data.internalCode,
-    isbn: data.isbn,
+    internalCode: internalCode,
     quantity: data.quantity,
-    publicationYear: data.publicationYear,
-    edition: data.edition,
     imageUrl: data.imageUrl,
-    status: data.status,
+    status: "activo",
     deletedAt: null,
+    // Atributos opcionales de libro
+    authorPrice: data.authorPrice ?? null,
+    numberPages: data.numberPages ?? null,
+    weight: data.weight ?? null,
+    dimensions: data.dimensions ?? null,
+    suggestedAges: data.suggestedAges ?? null,
+    isbn: data.isbn ?? null,
+    publicationYear: data.publicationYear ?? null,
+    edition: data.edition ?? null,
+    author: data.author ?? null,
+    format: data.format ?? null,
+    genre: data.genre ?? null,
+    language: data.language ?? null,
   };
-
-  const db = await connectToDatabase();
-  const { Product } = db;
   const newProduct = await Product.create(prodDB);
 
   return { data: newProduct };
@@ -55,17 +66,17 @@ const getProductArticlesFunction: SilmaAPIFunction = async () => {
   const db = await connectToDatabase();
   const { Product } = db;
   const rawProductArticle = await Product.findAll({
-        attributes: [
-          "type",
-          "title",
-          "synopsis",
-          "quantity",
-          "salesPrice",
-          "internalCode",
-          "imageUrl",
-          "status"
-        ],
-    where: { deletedAt: null, type: "article"},
+    attributes: [
+      "type",
+      "title",
+      "synopsis",
+      "quantity",
+      "salesPrice",
+      "internalCode",
+      "imageUrl",
+      "status",
+    ],
+    where: { deletedAt: null, type: "article" },
   });
   const productArticles = rawProductArticle.map((rawProduct) =>
     rawProduct.get({ plain: true })
@@ -80,29 +91,29 @@ const getProductBooksFunction: SilmaAPIFunction = async () => {
   const db = await connectToDatabase();
   const { Product } = db;
   const rawProductBook = await Product.findAll({
-        attributes: [
-          "type",
-          "title",
-          "author",
-          "synopsis",
-          "quantity",
-          "salesPrice",
-          "authorPrice",
-          "genre",
-          "language",
-          "format",
-          "numberPages",
-          "suggestedAges",
-          "weight",
-          "dimensions",
-          "internalCode",
-          "isbn",
-          "publicationYear",
-          "edition",
-          "imageUrl",
-          "status"
-        ],
-    where: { deletedAt: null, type: "book"},
+    attributes: [
+      "type",
+      "title",
+      "author",
+      "synopsis",
+      "quantity",
+      "salesPrice",
+      "authorPrice",
+      "genre",
+      "language",
+      "format",
+      "numberPages",
+      "suggestedAges",
+      "weight",
+      "dimensions",
+      "internalCode",
+      "isbn",
+      "publicationYear",
+      "edition",
+      "imageUrl",
+      "status",
+    ],
+    where: { deletedAt: null, type: "book" },
   });
   const productBooks = rawProductBook.map((rawProduct) =>
     rawProduct.get({ plain: true })
