234bbe6344287e137cb42d3048fe1daa944172fe
gpaez-ol <tavo@outlandhq.com>
2023-05-21 16:32:38 -0600

    feat: Added Location-InOrder Relationship as well as changing id from a number to a string type

--- silma-back/database/models/location.model.ts
+++ silma-back/database/models/location.model.ts
@@ -2,37 +2,37 @@ import { DataTypes, ModelDefined, Sequelize } from "sequelize";
 import { EntityAttributes } from "./base/entity.model";
 
 export type LocationAttributes = {
-    id?: number;
-    title: string;
-    description: string;
+  id?: string;
+  title: string;
+  description: string;
 } & EntityAttributes;
 
 export type LocationCreationAttributes = LocationAttributes;
 type GetModel = (
-    sequelize: Sequelize
+  sequelize: Sequelize
 ) => ModelDefined<LocationAttributes, LocationCreationAttributes>;
 
 export const LocationModel: GetModel = (sequelize: Sequelize) => {
-    const Model: ModelDefined<LocationAttributes, LocationCreationAttributes> =
+  const Model: ModelDefined<LocationAttributes, LocationCreationAttributes> =
     sequelize.define("Location", {
-        id: {
-            primaryKey: true,
-            type: DataTypes.UUID,
-            allowNull: false,
-            defaultValue: DataTypes.UUIDV4
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        deletedAt: {
-            type: new DataTypes.DATE(),
-            allowNull: true
-        }
+      id: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: new DataTypes.DATE(),
+        allowNull: true,
+      },
     });
-    return Model;
+  return Model;
 };
