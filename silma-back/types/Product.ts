83667a111fb9d786590d6e953240940d1fc8750a
JuanPabdl <a00825316@itesm.mx>
2023-05-22 14:43:11 -0600

    fixed product edit validation

--- silma-back/types/Product.ts
+++ silma-back/types/Product.ts
@@ -82,6 +82,56 @@ export const ProductCreateSchema = Joi.object<ProductCreate>({
   language: Joi.string().optional().allow(null),
 });
 
+export type ProductUpdate = {
+  id: string;
+  internalCode: string;
+  status: string;
+  title: string;
+  type: ProductType;
+  synopsis: string;
+  //quantity: number; //ELIMINAR
+  salesPrice: number;
+  imageUrl: string;
+  // Atributos de libro opcionales
+  authorPrice?: number;
+  numberPages?: number;
+  weight?: number;
+  dimensions?: string;
+  suggestedAges?: string;
+  isbn?: string;
+  publicationYear?: number;
+  edition?: string;
+  author?: string;
+  format?: ProductFormat;
+  genre?: ProductGenre;
+  language?: ProductLanguage;
+}
+
+export const ProductUpdateSchema = Joi.object<ProductUpdate>({
+  id: Joi.string().required(),
+  internalCode: Joi.string().required(),
+  status: Joi.string(),
+  //quantity: Joi.number().min(0), //ELIMINAR
+  salesPrice: Joi.number().min(0),
+  title: Joi.string(),
+  type: Joi.string(),
+  synopsis: Joi.string(),
+  imageUrl: Joi.string(),
+  //Atributos de libro opcionales
+  authorPrice: Joi.number().optional().allow(null).min(0),
+  numberPages: Joi.number().optional().allow(null).min(1),
+  weight: Joi.number().optional().allow(null).min(1),
+  dimensions: Joi.string().optional().allow(null),
+  suggestedAges: Joi.string().optional().allow(null),
+  isbn: Joi.string().optional().allow(null),
+  publicationYear: Joi.number().optional().allow(null),
+  edition: Joi.string().optional().allow(null),
+  author: Joi.string().optional().allow(null),
+  format: Joi.string().optional().allow(null),
+  genre: Joi.string().optional().allow(null),
+  language: Joi.string().optional().allow(null),
+})
+
 export const productTypes = ["book", "article"] as const;
 export type ProductType = (typeof productTypes)[number];
 
