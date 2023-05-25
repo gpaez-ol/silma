0fb9415ca18f57511dc6bb102847e039ac901b78
gpaez-ol <tavo@outlandhq.com>
2023-05-25 18:46:25 -0500

    feat: Added QueryClient and a default Bse URL to App

--- silma-front/src/index.tsx
+++ silma-front/src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import axios from 'axios';
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+axios.defaults.baseURL = process.env.API_URL ?? "http://localhost:3000/local/";
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
